283a1c73018ba6e311543343fbf3eb6c0f991d76
pealpool <pealpool@gmail.com>
2022-02-23 17:44:17 +0800

    aaa

--- src/components/MyItem.vue
+++ src/components/MyItem.vue
@@ -1,13 +1,66 @@
 <template>
-
+  <li>
+    <label>
+      <input type="checkbox" :checked="todo.done"/>
+      <span>{{ todo.todoName }}</span>
+    </label>
+    <button class="btn btn-danger" @click="deleteItemId(todo.id)">删除</button>
+  </li>
 </template>
 
 <script>
 export default {
-  name: "MyItem"
+  name: "MyItem",
+  props: ['todo','deleteItem'],
+  methods: {
+    deleteItemId(id){
+      this.deleteItem(id)
+    }
+  }
 }
 </script>
 
 <style scoped>
+/*item*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
 
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  float: right;
+  display: none;
+  margin-top: 3px;
+}
+
+li:hover button {
+  display: block;
+}
+
+li:hover {
+  background: #f5f5f5;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
 </style>
--- src/components/MyList.vue
+++ src/components/MyList.vue
@@ -1,13 +1,36 @@
 <template>
-
+  <ul class="todo-main">
+    <MyItem
+        v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj" :deleteItem="deleteItem"/>
+<!--    <MyItem />-->
+  </ul>
 </template>
 
 <script>
+import MyItem from "@/components/MyItem";
+
 export default {
-  name: "MyList"
+  name: "MyList",
+  props:['todos','deleteItem'],
+  components: {MyItem}
 }
 </script>
 
 <style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
 
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
 </style>
