2063d609d2bedfc32cd9cc0d2c6c9cadb6bc5754
pealpool <pealpool@gmail.com>
2022-02-23 15:46:33 +0800

    124

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import MyHeader from "@/components/MyHeader";
+  import MyFooter from "@/components/MyFooter";
+  import MyList from "@/components/MyList";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+  export default {
+    name:'App',
+    components:{MyHeader,MyFooter,MyList}
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
